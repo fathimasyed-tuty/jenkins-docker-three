d5bf53b85d8d48186f6e5bf6a348b1fcabb93899
Syed Ali Fathima A <syedalifathima.a@publicissapient.com>
2022-11-08 20:07:34 +0530

    third commit

---
+++ src/main/java/com/example/jenkinsdockerthree/JenkinsDockerThreeApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsdockerthree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsDockerThreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsDockerThreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jenkinsdockerthree/JenkinsDockerThreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsdockerthree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsDockerThreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
